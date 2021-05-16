5b3e4de3a69116f0c7bfe1c632fd6928af6d4228
KayoshT <kaythiyaga@gmail.com>
2021-05-16 15:38:52 +1000

    finished frontend

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -2,8 +2,10 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import { useState } from 'react'
+import AddTask from './components/AddTask'
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
   const[tasks, setTasks] = useState([
     {
       id: 1, 
@@ -31,12 +33,22 @@ function App() {
   }
   // Toggle Reminder
   const toggleReminder = (id) => {
-    console.log(id)
+    setTasks(
+      tasks.map((task) => 
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    )
   }
-
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random()* 10000) + 1
+    const newTask = {id, ...task }
+    setTasks([...tasks, newTask])
+  }
   return (
     <div className='container'>
-      <Header  />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask}/>}
       {tasks.length > 0 ? (
       <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
       ) : (
